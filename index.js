f06654236e7524d6fadb68cc4ab4deab2d306c36
mohehexd <dumblemoe@gmail.com>
2020-10-30 14:16:14 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code here!
+// Removes id main completely
+document.querySelector("main#main").remove();
+
+// Creates New Element inside of a variable
+var newHeader = document.createElement("H1");
+
+// Creates new Header Class & Id (id is "victory")
+newHeader.class = "victory"
+newHeader.id = "victory"
+
+//creates new string inside of header (element/node?)
+newHeader.innerHTML = "Mo is the champion of th world. Booyah pizza Party"
